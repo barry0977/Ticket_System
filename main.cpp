852353f5790a67e33da2d580e621694301ab7f66
Barry <2411557145@qq.com>
2024-06-02 02:14:02 +0800

    还原

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ Userinf userinf;
 Traininf traininf;
 int main()
 {
-    freopen("7.in","r",stdin);
-    freopen("out.txt","w",stdout);
+//    freopen("7.in","r",stdin);
+//    freopen("out.txt","w",stdout);
     std::string input;
     while(1)
     {
